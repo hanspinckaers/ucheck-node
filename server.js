dc9b12dbd97aaa7311165df5073c1b852bffda26
Hans Pinckaers <hans.pinckaers@gmail.com>
2011-12-18 11:23:37 +0100

    Server.js aangepast voor no.de compabiliteit.

--- server.js
+++ server.js
@@ -283,7 +283,7 @@ if (!module.parent)
 	// 	app.listen(3000);
 	// }
 
-	app.listen(process.env.PORT || 8001)
+	app.listen(process.env.PORT || 3000)
 	
 	console.log("Express server listening on port %d", app.address().port);
 
